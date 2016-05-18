d616578d68bb27a8d7e6ee73d7af56a353e049f9
GaelBy <g.benay@hotmail.fr>
2016-05-18 10:24:02 +0200

    Mise à jour index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php	
-	$access = array(/* Tab contenant les pages disponibles du site */);
+	session_start();
+	$access = array('home', 'login', 'register', 'admin_article', 'article', 'commentaire', 'logout');
 	$page = 'home' /*page courante : home par default*/ ;
 	if (isset($_GET['page']))
 	{
 		if (in_array($_GET['page'], $access))
 			$page = $_GET['page'];
 	}
+	$access_traitement = array('login', 'register', 'admin_article', 'commentaire', 'logout');
+	if (in_array($page, $access_traitement))
+		require('apps/traitement_'.$page.'.php');// apps/traitement_login.php ou apps/traitement_register.php ou apps/traitement_contact.php
 	require 'apps/skel.php';
 ?>
