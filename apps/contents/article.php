eca0d3d626e5f99e0b647b5a8ba412a29adc5e39
MarloneB <bertrandmarlone@gmail.com>
2016-05-19 14:05:56 +0200

    maj article

--- apps/contents/article.php
+++ apps/contents/article.php
@@ -1,13 +1,17 @@
 <?php
-
-	$id = $_GET['id'];
-	$query = 'SELECT id, author, title, content, image, "date" FROM articles WHERE id = $id';
-	$res = mysqli_query($link, $query);
-
-	while($_GET['id'] == $id)
+	if(isset($_GET['id']))
 	{
+		$id = $_GET['id'];
+		$query = 'SELECT id, author, title, content, image, "date" FROM articles WHERE id = ?';
+		
+		$req = mysqli_prepare($link, $query);
+		mysqli_stmt_bind_param($req, "i", $id);
+		mysqli_stmt_execute($req);
+		mysqli_stmt_bind_result($req, $id, $author, $title, $content, $image, $createDate);
+		mysqli_stmt_fetch($req);
+		mysqli_stmt_close($req);
+
 		require'views/contents/article.phtml';
 	}
-	$error = 'Article inconnu';
-
+$error = 'Article inconnu';
 ?>
