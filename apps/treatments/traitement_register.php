88f20e82d57d66031e5c23d764b9ccc847f43898
Arteast <arteast.academy@gmail.com>
2016-05-19 11:10:37 +0200

    maj indentation

--- apps/treatments/traitement_register.php
+++ apps/treatments/traitement_register.php
@@ -1,39 +1,40 @@
 <?php
-
-if (isset($_POST['login'], $_POST['email'], $_POST['confirmEmail'], $_POST['password'], $_POST['confirmPassword']))
-{
-
-	$login = $_POST['login'];
-	$email = $_POST['email'];
-	$confirmEmail = $_POST['confirmEmail'];
-	$password = $_POST['password'];
-	$confirmPassword = $_POST['confirmPassword'];
-
-	if (strlen($login) <2)
-		$error = 'Votre pseudo est trop court ! (< 2 caractères)';
-	else if (strlen($login) >32)
-		$error = 'Votre pseudo est trop long ! (> 32 caractères)';
-
-	if ($email != $confirmEmail)
-		$error = 'E-mail différents !';
-	else if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-		$error = 'Email non valide';
-
-	if ($password != $confirmPassword)
-		$error = 'Mots de passe différents !';
-	else if (strlen($password) <4)
-		$error = 'password trop court ! (au moins 4 caractères)';
-
-	if (empty($error))
+	if (isset($_POST['login'], $_POST['email'], $_POST['confirmEmail'], $_POST['password'], $_POST['confirmPassword']))
 	{
-		$password = sha1($password);
-		$query = 'INSERT INTO users(login, email, password) VALUES (?,?,?)';
-		$req = mysqli_prepare($link, $query);
-		mysqli_stmt_bind_param($req, "sss", $login, $email, $password);
-		mysqli_stmt_execute($req);
-		mysqli_stmt_close($req);
-		header('Location: index.php?page=login');
-		exit;
+
+		$login = $_POST['login'];
+		$email = $_POST['email'];
+		$confirmEmail = $_POST['confirmEmail'];
+		$password = $_POST['password'];
+		$confirmPassword = $_POST['confirmPassword'];
+
+		if (strlen($login) <2)
+			$error = 'Votre pseudo est trop court ! (< 2 caractères)';
+		else if (strlen($login) >32)
+			$error = 'Votre pseudo est trop long ! (> 32 caractères)';
+
+		if ($email != $confirmEmail)
+			$error = 'E-mail différents !';
+		else if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+			$error = 'Email non valide';
+
+		if ($password != $confirmPassword)
+			$error = 'Mots de passe différents !';
+		else if (strlen($password) <4)
+			$error = 'password trop court ! (au moins 4 caractères)';
+
+		if (empty($error))
+		{
+			$password = sha1($password);
+			$query = 'INSERT INTO users(login, email, password) VALUES (?,?,?)';
+			$req = mysqli_prepare($link, $query);
+
+			mysqli_stmt_bind_param($req, "sss", $login, $email, $password);
+			mysqli_stmt_execute($req);
+			mysqli_stmt_close($req);
+			
+			header('Location: index.php?page=login');
+			exit;
+		}
 	}
-}
 ?>
